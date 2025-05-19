9f23ee78498e94312845c978088b84eb6652f6c9
peshovec <jlesev@gmail.com>
2025-05-19 18:47:47 +0300

    move to littlefs

--- main/webUI.cpp
+++ main/webUI.cpp
@@ -23,7 +23,9 @@
 */
 #include "User_config.h"
 #if defined(ZwebUI) && defined(ESP32)
-#  include <SPIFFS.h>
+#  include <ArduinoJson.h>
+#  include <LittleFS.h>
+#  define SPIFFS LittleFS
 #  include <WebServer.h> // Docs for this are here - https://github.com/espressif/arduino-esp32/tree/master/libraries/WebServer
 #  include <WiFi.h>
 
